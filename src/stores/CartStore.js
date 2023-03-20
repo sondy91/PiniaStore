0367fcbba1ec2b34505d13aadec211793ffed8f5
Austin Sonderman <6412733+sondy91@users.noreply.github.com>
2023-03-20 01:21:02 -0500

    Fixing cart display and using dynamic getters for count

--- src/stores/CartStore.js
+++ src/stores/CartStore.js
@@ -1,5 +1,5 @@
 import { defineStore } from "pinia";
-
+import { groupBy } from "lodash";
 export const useCartStore = defineStore("CartStore", {
   state: () => {
     return {
@@ -16,6 +16,8 @@ export const useCartStore = defineStore("CartStore", {
   },
   getters: {
     count: (state) => state.items.length,
-    isEmpty: (state) => state.count === 0
+    isEmpty: (state) => state.count === 0,
+    grouped: (state) => groupBy(state.items, (item) => item.name),
+    groupCount: (state) => (name) => state.grouped[name].length,
   },
 });
